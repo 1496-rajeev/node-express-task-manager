a36c58a51e99f45bed379fed8b870181d4c6bad5
1496-rajeev <1496piku@gmail.com>
2022-06-04 13:50:35 +0530

    database connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { connectDB } from './db/connect.js';
 import express from 'express';
 import { router as tasks } from './routes/tasks.js';
 
@@ -12,6 +13,19 @@ app.use('/v1/tasks', tasks);
 //port
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`server is listening on port ${PORT}`);
-});
+//connect db and start server
+const start = async () => {
+  try {
+    //connect db
+    await connectDB();
+
+    // listen server
+    app.listen(PORT, () => {
+      console.log(`server is listening on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
